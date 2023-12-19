a0924e2ee6e5fab656ab14df61f3b49e1921f822
alisabiya18 <101519925+alisabiya18@users.noreply.github.com>
2023-12-19 17:47:21 +0545

    const and let

---
+++ index.js
@@ -0,0 +1,11 @@
+console.log("hello world");
+//Es5
+var person = "sabiha";
+
+//ES6
+//allow us to reassign the variable
+let person1="ram";
+//doesnt allow to change the variable
+// const persion2="aalu";
+console.log(person);
+console.log(person1);
