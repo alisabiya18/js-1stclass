89cdb8058410e090f3fec02d2ddd90a682d25a73
alisabiya18 <101519925+alisabiya18@users.noreply.github.com>
2023-12-20 16:45:49 +0545

    index.html

--- index.js
+++ index.js
@@ -1,17 +1,23 @@
-console.log("hello world");
-//Es5
-var person = "sabiha";
+// console.log("hello world");
+// //Es5
+// var person = "sabiha";
 
-//ES6
-//allow us to reassign the variable
-let person1 = "ram";
-//doesnt allow to change the variable
-// const persion2="aalu";
-console.log(person);
-console.log(person1);
-//interaction
-alert("hello");
-const form = prompt("what is your name?");
-const form1 = confirm("are you okay?");
-console.log(form, form1);
-console.log("using git command");
+// //ES6
+// //allow us to reassign the variable
+// let person1 = "ram";
+// //doesnt allow to change the variable
+// // const persion2="aalu";
+// console.log(person);
+// console.log(person1);
+// //interaction
+// alert("hello");
+// const form = prompt("what is your name?");
+// const form1 = confirm("are you okay?");
+// console.log(form, form1);
+// console.log("using git command");
+//selfdone
+// alert("oyee");
+// let name1 = prompt("what is your name");
+// console.log(name1);
+// let fine1 = confirm("are you okay");
+// console.log(fine1);
