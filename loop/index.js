acb1f46940a8e6d0de260761a42ac6902e56ab7d
alisabiya18 <101519925+alisabiya18@users.noreply.github.com>
2023-12-22 18:20:25 +0545

    adding loop

---
+++ loop/index.js
@@ -0,0 +1,21 @@
+// for(let i=0;i<=100;i++)
+// {
+// console.log(i);
+// }
+// let i = 0;
+// while (i <= 100) {
+//   console.log(i);
+//   i++;
+// }
+// let i=0;
+// do{
+   
+//     console.log(i);
+//     i++
+// }while(i<=100);
+let value=prompt("enter number for which you want table");
+for(let i=0;i<=10;i++)
+{
+    let mul=value*i;
+    console.log(mul);
+}
